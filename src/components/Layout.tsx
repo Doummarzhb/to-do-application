2b9a89c5c3dd6d5d4e3ffd77d7fbb35f3bb9c985
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2025-08-15 21:58:57 +0300

    D

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,8 +4,8 @@ import type {LayoutProps} from '../types/task';
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#23262C] font-poppins">
-      <div className="bg-[#23262C] text-white w-full max-w-[1280px]  min-h-screen   rounded-[32px] overflow-y-auto mb-8 ">
+    <div className="min-h-screen flex items-center justify-center bg-[#23262C] font-poppins overflow-hidden">
+      <div className="bg-[#23262C] text-white w-full max-w-[1280px]  min-h-screen   rounded-[32px] overflow-hidden mb-8 ">
         
         <Header />
         
